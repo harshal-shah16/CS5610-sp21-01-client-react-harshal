fcff553c5211e2deacb5d9ebab94168a65c6d710
harshal-shah16 <harshalshah1687@gmail.com>
2021-04-18 03:54:39 -0400

    A8 - connected to mongoDB

--- src/services/quiz-service.js
+++ src/services/quiz-service.js
@@ -9,7 +9,19 @@ const findQuizById = (qid) => {
       .then(response => response.json())
 }
 
+const submitQuiz = (quizId, questions) => {
+   fetch(`http://localhost:3000/api/quizzes/${quizId}/attempts`, {
+     method: 'POST',
+     body: JSON.stringify(questions),
+     headers: {
+       'content-type': 'application/json'
+     }
+   }).then(response => response.json())
+     .then(result => console.log(result))
+  }
+  
+
 const api = {
-   findAllQuizzes, findQuizById
+   findAllQuizzes, findQuizById, submitQuiz
 }
 export default api;
