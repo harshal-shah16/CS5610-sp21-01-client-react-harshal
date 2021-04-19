2f539ec10701aa2c3ce78f8cfdb2d609979178ca
harshal-shah16 <harshalshah1687@gmail.com>
2021-04-19 04:00:48 -0400

    env updated

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const QUIZZES_URL = process.env.NODE_SERVER_URL;
+const QUIZZES_URL = process.env.REACT_APP_NODE_SERVER_URL;
 const findQuestionsForQuiz = (qid) => {
    return fetch(`${QUIZZES_URL}/${qid}/questions`)
       .then(response => response.json())
